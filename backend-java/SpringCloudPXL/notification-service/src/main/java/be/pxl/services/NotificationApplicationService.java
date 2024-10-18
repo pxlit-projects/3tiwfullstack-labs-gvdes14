0d1ccd83978a5cbfcc0b9b0534a43004d99409e5
Gregory Vervoort <gregory.vervoort@student.pxl.be>
2024-10-18 15:36:55 +0200

    Discovery service ok for all microservices updated ports + service names

--- backend-java/SpringCloudPXL/notification-service/src/main/java/be/pxl/services/NotificationApplicationService.java
+++ backend-java/SpringCloudPXL/notification-service/src/main/java/be/pxl/services/NotificationApplicationService.java
@@ -2,12 +2,15 @@ package be.pxl.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * NotificationApplicationService
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationApplicationService
 {
     public static void main( String[] args )
